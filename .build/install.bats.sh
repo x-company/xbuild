89467706d8f267faf0a21d8634bdafd06a847b3f
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-07 18:17:20 +0200

    Update Dockerfiles

--- .build/install.bats.sh
+++ .build/install.bats.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-source /usr/local/include/xbuild
+# Load the xBuild System
+source /usr/local/include/xbuild/loader
 
-header "Prepare Docker Image for Bash Testing"
+# Load the Environment Variables to the current Session
+loadvars
 
-log "Install Bats ..."
-install bats
+# Prepare the Image
+prepare
+
+# Write an Header and Log Information
+header "Lets start with xbuild"
+
+# Install Nano Editor
+log "Install bats and shellcheck..."
+install bats shellcheck
+
+# Cleanup the Build
+cleanup
+
+# Persist Environment Variables
+savevars
+
+log "That's it. xBuild has finished his work. Have a nice Day"
