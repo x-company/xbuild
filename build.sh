9fb06fb51454bc6e6d8a84781abca4a2d3d9bb5a
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-14 17:56:03 +0200

    Automatic Build Commit

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-
-echo "$(sed -e "s/__XBUILD_BUILD_DATE__/$(date -u +'%Y-%m-%dT%H:%M:%SZ')/g" ./Dockerfile.tmpl)" > ./Dockerfile
-echo "$(sed -e "s/__XBUILD_VCS_REF__/$(git rev-parse --short HEAD)/g" ./Dockerfile)" > ./Dockerfile
-echo "$(sed -e "s/__XBUILD_VERSION__/npm_package_version/g" ./Dockerfile)" > ./Dockerfile
