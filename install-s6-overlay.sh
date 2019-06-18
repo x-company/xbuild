e98069054d08686f25a1493d4fad1df9bbd18c6c
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-19 00:58:46 +0200

    Update Install Script for s6

--- install-s6-overlay.sh
+++ install-s6-overlay.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+s6ver="1.22.1.0"
+
 mkdir -p ./tmp
-cd ./tmp
-wget https://github.com/just-containers/s6-overlay/releases/download/v1.22.1.0/s6-overlay-amd64.tar.gz
+cd ./tmp || exit 1
+wget "https://github.com/just-containers/s6-overlay/releases/download/v$s6ver/s6-overlay-amd64.tar.gz"
 tar xzf ./s6-overlay-amd64.tar.gz -C ../src/
 cd ..
 rm -rf ./tmp
