a35840c0b6344c6cd0ef3abd38842a34af07b515
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-19 07:36:28 +0200

    Correct xbuild commands for s6 supvervisor

--- install-s6-overlay.sh
+++ install-s6-overlay.sh
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 s6ver="1.22.1.0"
+socklogver="3.1.0-2"
 
 mkdir -p ./tmp
 cd ./tmp || exit 1
 wget "https://github.com/just-containers/s6-overlay/releases/download/v$s6ver/s6-overlay-amd64.tar.gz"
 tar xzf ./s6-overlay-amd64.tar.gz -C ../src/
+
+wget "https://github.com/just-containers/socklog-overlay/releases/download/v$socklogver/socklog-overlay-amd64.tar.gz"
+tar xzf ./socklog-overlay-amd64.tar.gz -C ../src/
+
 cd ..
 rm -rf ./tmp
+
