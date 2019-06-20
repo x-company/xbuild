54a1653b90155809772e1378db7d9be32d545d86
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-20 23:37:45 +0200

    Update Install for socklog

--- install-s6-overlay.sh
+++ install-s6-overlay.sh
@@ -10,13 +10,13 @@ wget "https://github.com/just-containers/s6-overlay/releases/download/v$s6ver/s6
 tar xzf ./s6-overlay-amd64.tar.gz -C ../src/xcompany/xbuild/rootfs
 
 wget "https://github.com/just-containers/socklog-overlay/releases/download/v$socklogver/socklog-overlay-amd64.tar.gz"
-tar xzf ./socklog-overlay-amd64.tar.gz -C ../src/xcompany/xbuild/rootfs
+tar xzf ./socklog-overlay-amd64.tar.gz -C ../src/xcompany/xbuild/build/rootfs
 
-mv ../src/xcompany/xbuild/rootfs/etc/services.d/socklog/run ../src/xcompany/xbuild/services/socklog/socklog.run
-mv ../src/xcompany/xbuild/rootfs/etc/services.d/socklog/log/run ../src/xcompany/xbuild/services/socklog/log/socklog.run
-mv ../src/xcompany/xbuild/rootfs/etc/cont-init.d/~-socklog ../src/xcompany/xbuild/services/socklog/socklog.init
-rm -rf ../src/xcompany/xbuild/rootfs/etc/cont-init.d/~-socklog
-rm -rf ../src/xcompany/xbuild/rootfs/etc/services.d/socklog
+mv ../src/xcompany/xbuild/build/rootfs/etc/services.d/socklog/run ../src/xcompany/xbuild/build/services/socklog/socklog.run
+mv ../src/xcompany/xbuild/build/rootfs/etc/services.d/socklog/log/run ../src/xcompany/xbuild/build/services/socklog/log/socklog.run
+mv ../src/xcompany/xbuild/build/rootfs/etc/cont-init.d/~-socklog ../src/xcompany/xbuild/build/services/socklog/socklog.init
+rm -rf ../src/xcompany/xbuild/build/rootfs/etc/cont-init.d/~-socklog
+rm -rf ../src/xcompany/xbuild/build/rootfs/etc/services.d/socklog
 
 cd ..
 rm -rf ./tmp
