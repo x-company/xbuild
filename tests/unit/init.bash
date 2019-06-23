0ed342f102c67e2eb182ee39f9a66e801876b4d1
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-23 21:16:11 +0200

    Update Unit Test

--- tests/unit/init.bash
+++ tests/unit/init.bash
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-
-# shellcheck source=../../usr/local/include/xbuild/loader
-source /usr/local/include/xbuild/loader
-
-# Disable Validation for unbound Vars
-set +u
-
