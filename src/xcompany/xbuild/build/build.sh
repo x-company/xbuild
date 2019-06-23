d565174a0850f3865db98b2f29dd02fd92b9507a
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-23 15:31:33 +0200

    Remove examples

--- src/xcompany/xbuild/build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# This is the main Build File for your Docker Image
-#
-# POWERTIP: Use Snippet xb-build to create a Sample
-# Hint: Look also for other Snippets with the Prefix 'xb-...'
-
