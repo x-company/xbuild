378619f52c3c5e782dff413cbb7d5706620b7b4c
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-23 15:16:43 +0200

    Commit Project Files

--- src/xcompany/xbuild/build/build.sh
+++ src/xcompany/xbuild/build/build.sh
@@ -6,26 +6,3 @@
 # POWERTIP: Use Snippet xb-build to create a Sample
 # Hint: Look also for other Snippets with the Prefix 'xb-...'
 
-
-# Load the xBuild System
-source /usr/local/include/xbuild/loader
-
-# Load the Environment Variables to the current Session
-loadvars
-
-# POWERTIP: Use the Snippet xb-prepare and xb-prepare-lang to Prepare your Image
-
-
-header "Prepare Services for Install ..."
-services=
-
-header "Build Services ..."
-build --services "$services"
-
-# Persist Environment Variables
-savevars
-
-# Cleanup the Build and the Image. It should called when you finished your Work
-cleanup
-
-header "That's it. xBuild has finished his work. Have a nice Day"
