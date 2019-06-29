d8db295c3f357ca672535ab757c7ec05db7d800d
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-06-29 19:13:26 +0200

    Cleanup Source Folder

--- .devcontainer/cleanup-source.sh
+++ .devcontainer/cleanup-source.sh
@@ -2,12 +2,13 @@
 
 cd ./.devcontainer || exit 1
 
-rootfs="../src/xcompany/xbuild/rootfs/"
+rootfs="../src/xcompany/xbuild/rootfs"
 
-rm -rf "${rootfs:?}/etc/xbuild/env.d/*"
+rm -rf "${rootfs:?}/etc/xbuild/env.d/"
 rm -rf "${rootfs:?}/etc/xbuild/sources.list"
-rm -rf "${rootfs:?}/var/cache/xbuild/packages/*"
-rm -rf "${rootfs:?}/var/log/xbuild/*"
-rm -rf "${rootfs:?}/var/run/xbuild/*"
+rm -rf "${rootfs:?}/var/cache/"
+rm -rf "${rootfs:?}/var/local/"
+rm -rf "${rootfs:?}/var/log/xbuild/"
+rm -rf "${rootfs:?}/var/run/"
 
 cd ..
