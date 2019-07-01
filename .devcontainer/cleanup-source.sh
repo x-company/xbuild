fad53cde5cb30fba1f1be9b7bb4a0a5775cec5ea
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-07-01 20:20:27 +0200

    Copy all env Vars to s6

--- .devcontainer/cleanup-source.sh
+++ .devcontainer/cleanup-source.sh
@@ -4,6 +4,7 @@ cd ./.devcontainer || exit 1
 
 rootfs="../src/xcompany/xbuild/rootfs"
 
+rm -rf "${rootfs:?}/etc/s6/init/env/*"
 rm -rf "${rootfs:?}/etc/xbuild/env.d/"
 rm -rf "${rootfs:?}/etc/xbuild/sources.list"
 rm -rf "${rootfs:?}/var/cache/"
@@ -11,4 +12,5 @@ rm -rf "${rootfs:?}/var/local/"
 rm -rf "${rootfs:?}/var/log/xbuild/"
 rm -rf "${rootfs:?}/var/run/"
 
+echo "/usr/bin:/usr/sbin:/bin:/sbin" > "${rootfs:?}/etc/s6/init/env/PATH"
 cd ..
