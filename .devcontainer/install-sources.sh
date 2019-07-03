59b510d9470eca52fcf0bff60753e7542ab7baa4
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-07-03 18:23:53 +0200

    Add consul as Default Service to the Image

--- .devcontainer/install-sources.sh
+++ .devcontainer/install-sources.sh
@@ -3,13 +3,22 @@
 
 s6ver="1.22.1.0"
 socklogver="3.1.0-2"
+consulver="1.5.2"
 
 mkdir -p ./update
-
 cd ./update || exit 1
 
+# Download Sources
 if [ ! -f ./s6-overlay-amd64.tar.gz ]; then
-    wget "https://github.com/just-containers/s6-overlay/releases/download/v$s6ver/s6-overlay-amd64.tar.gz"
+    wget "https://github.com/just-containers/s6-overlay/releases/download/v${s6ver}/s6-overlay-amd64.tar.gz"
+fi
+
+if [ ! -f ./s6-overlay-amd64.tar.gz ]; then
+    wget "https://github.com/just-containers/s6-overlay/releases/download/v${s6ver}/s6-overlay-amd64.tar.gz"
+fi
+
+if [ ! -f "./consul_${consulver}_linux_amd64.zip" ]; then
+    wget "https://releases.hashicorp.com/consul/${consulver}/consul_${consulver}_linux_amd64.zip"
 fi
 
 # Cleanup old
@@ -22,17 +31,14 @@ rm -rf "${rootfs:?}/etc/s6/"
 rm -rf "${rootfs:?}/etc/leapsecs.dat"
 rm -rf "${rootfs:?}/libexec"
 rm -rf "${rootfs:?}/usr/bin"
-
-tar -xzf ./s6-overlay-amd64.tar.gz -C ${rootfs:?}
-
-if [ ! -f ./socklog-overlay-amd64.tar.gz ]; then
-    wget "https://github.com/just-containers/socklog-overlay/releases/download/v$socklogver/socklog-overlay-amd64.tar.gz"
-fi
-
 rm -rf "${rootfs:?}/var/log"
 
+# Unpack Downloads
+tar -xzf ./s6-overlay-amd64.tar.gz -C ${rootfs:?}
 tar -xzf ./socklog-overlay-amd64.tar.gz -C ${rootfs:?}
+unzip "./consul_${consulver}_linux_amd64.zip" -d ${rootfs:?}/usr/local/bin
 
+# Move Socklog Service Files to Service Dir
 mv -f "${rootfs:?}/etc/services.d/socklog/run" "${servicedir}/socklog/socklog.run"
 mv -f "${rootfs:?}/etc/services.d/socklog/log/run" "${servicedir}/socklog/log/socklog.run"
 mv -f "${rootfs:?}/etc/cont-init.d/~-socklog" "${servicedir}/socklog/99-socklog.init"
