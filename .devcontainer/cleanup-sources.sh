485206e4a7a89f8a26e60f8a063689b1d572a54a
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-07-05 12:29:09 +0200

    Update Project Helper Files

--- .devcontainer/cleanup-sources.sh
+++ .devcontainer/cleanup-sources.sh
@@ -4,12 +4,25 @@ cd ./.devcontainer || exit 1
 
 rootfs="../src/xcompany/xbuild/rootfs"
 
-rm -rf "${rootfs:?}/etc/s6/init/env/*"
+rm -rf "${rootfs:?}/bin/"
+rm -rf "${rootfs:?}/etc/cont-finish.d/"
+rm -rf "${rootfs:?}/etc/cont-init.d/"
+rm -rf "${rootfs:?}/etc/fix-attrs.d/"
+rm -rf "${rootfs:?}/etc/s6/"
+rm -rf "${rootfs:?}/etc/services.d/"
+rm -rf "${rootfs:?}/etc/socklog.rules/"
+rm -rf "${rootfs:?}/etc/leapsecs.dat"
 rm -rf "${rootfs:?}/etc/xbuild/env.d/"
-rm -rf "${rootfs:?}/etc/xbuild/sources.list"
+rm -rf "${rootfs:?}/libexec/"
+rm -rf "${rootfs:?}/usr/bin/"
+rm -rf "${rootfs:?}/usr/local/bin/consul"
+rm -rf "${rootfs:?}/usr/local/bin/consul-template"
 rm -rf "${rootfs:?}/var/cache/"
 rm -rf "${rootfs:?}/var/local/"
+rm -rf "${rootfs:?}/var/log/socklog/"
 rm -rf "${rootfs:?}/var/log/xbuild/"
 rm -rf "${rootfs:?}/var/run/"
+rm -rf "${rootfs:?}/init"
+rm -rf "${rootfs:?}/etc/xbuild/sources.list" ||
 
 cd ..
